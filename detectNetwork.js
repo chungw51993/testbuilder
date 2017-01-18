b904cebef5a5328502e62396452d34ca4f7ab724
Jin Chung <chungw51993@gmail.com>
2017-01-17 21:52:47 -0600

    Created assert equal function to check

--- detectNetwork.js
+++ detectNetwork.js
@@ -1,8 +1,8 @@
-// Given a credit card number, this function should return a string with the 
+// Given a credit card number, this function should return a string with the
 // name of a network, like 'MasterCard' or 'American Express'
 // Example: detectNetwork('343456789012345') should return 'American Express'
 
-// How can you tell one card network from another? Easy! 
+// How can you tell one card network from another? Easy!
 // There are two indicators:
 //   1. The first few numbers (called the prefix)
 //   2. The number of digits in the number (called the length)
@@ -16,3 +16,12 @@ var detectNetwork = function(cardNumber) {
 };
 
 
+function assertEqual (actual, expected, testName) {
+  if (actual === expected) {
+    console.log('PASSED');
+  } else {
+    console.log('FAILED "' + testName + '" expected "' + expected +'" but got "' + actual + '"');
+  }
+}
+
+assertEqual (detectNetwork('343456789012345'), 'American Express', 'it passes if the card is American Express');
